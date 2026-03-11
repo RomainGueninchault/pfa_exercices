6781c857b6080da762d7f87f38a40203c0d078a3
Romain Gueninchault <romain.gueninchault@enseirb-matmeca.fr>
2026-03-11 08:36:32 +0100

    Sum exercise back to its original state

--- exos_C_with_cmake/arrays/sum/skeleton.c
+++ exos_C_with_cmake/arrays/sum/skeleton.c
@@ -1,4 +1,5 @@
 // Compute the sum of all elements in array 'arr' of length 'len'.
 // If the input is invalid (len <= 0), return EXIT_FAILURE.
 int sum(int len, int *arr) {
+
 }
--- sum/skeleton.c
+++ sum/skeleton.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-//Compute the max of an array 'arr' which length is 'len'
-// If the input is invalid, return EXIT_FAILURE
+//Compute the sum of two integers
 
-int sum(int len, int arr[len]){
+int sum(int a, int b){
 }
